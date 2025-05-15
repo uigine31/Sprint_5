aef816483724d40bc8ac285d5d9ee070d143c2b7
Evgeniy Smolovyi <uigine31@gmail.com>
2025-05-16 00:49:42 +0700

    Оставил только Sprint_5 в репозитории, удалил остальные папки из индекса

--- PycharmProjects/PythonProject2/test_salary.py
+++
@@ -1,7 +0,0 @@
-from salary import SalaryService
-
-class TestSalaryService:
-    def test_calculate_salary_when_under_limit(self):
-        actual = SalaryService.calculate_salary(50000)
-        expected = 2500
-        assert actual == expected
